6cb258d77d514679533ed6c33cd6967aab3e246b
Amanreet Bajwa <abajwa@mines.edu>
2011-04-09 12:08:46 -0600

    Fixed ticket 1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "abajwa@mines.edu"
+    from          "no-reply@high-lightning-781.com"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
