b8e980cacb15f94e16b27eea09fda5d8b3d5cf54
Andy Niccolai <aniccola@mines.edu>
2011-04-20 04:07:40 -0600

    progress made

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -2,10 +2,14 @@ module FavoritesHelper
   
   def favorite_button(destroyer)
     if (favorite = destroyer.favorites.find(:first, params[:user_id] = current_user.id))
-      render :partial => "/members/favorites/unfavorite_button", :locals => {:favorite => favorite}
+      render :partial => "/members/favorites/unfavorite_button", :locals => {:favorite => favorite, :destroyer => destroyer}
      else
       render :partial => "/members/favorites/favorite_button", :locals => {:destroyer => destroyer}
     end
+   end
+   
+   def favorite_div(destroyer)
+     "favorite_button#{destroyer.id}"
    end
    
 end
