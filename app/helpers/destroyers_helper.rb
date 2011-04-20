3930faf1298567ceca9049d38995858df691e661
Andy Niccolai <aniccola@mines.edu>
2011-04-19 21:53:49 -0600

    fixed function to get favorite

--- app/helpers/destroyers_helper.rb
+++ app/helpers/destroyers_helper.rb
@@ -1,7 +1,7 @@
 module DestroyersHelper
   
   def is_favorite?(destroyer)
-    if destroyer.favorites.exists?(:user_id => current_user.id)
+    if favorite = destroyer.favorites.find(:first, params[:user_id] = current_user.id)
          link_to(image_tag("icon_small_star.png", :title => "unfavorite"), members_favorite_path(destroyer), :method => :delete)
        else
          link_to(image_tag("icon_small_star_inactive.png", :title => "favorite"))
