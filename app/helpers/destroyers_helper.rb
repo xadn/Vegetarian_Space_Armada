b73e3ded319651e24324bde7262bad5bfb76ae96
Andy Niccolai <aniccola@mines.edu>
2011-04-21 15:54:57 -0600

    got more button working, for members only

--- app/helpers/destroyers_helper.rb
+++ app/helpers/destroyers_helper.rb
@@ -1,3 +1,19 @@
 module DestroyersHelper
 
+  def destroyer_div(destroyer)
+     "destroyer_description#{destroyer.id}"
+  end
+  
+  def more_link_div(destroyer)
+    "more_link#{destroyer.id}"
+  end
+  
+  def toggle_more(destroyer)
+    link_to_function("more...", :id => "more_link") do |page|
+       page[ destroyer_div(destroyer) ].toggle
+       page["#{destroyer_div(destroyer)}_more"].toggle
+       #page[ more_link_div(destroyer) ].replace_html "Show me less"
+    end
+  end
+
 end
