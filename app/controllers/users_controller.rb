f5e6bd4cf4bcb2b8083d5e93d6b6201cd2be3e97
Andy Niccolai <aniccola@mines.edu>
2011-04-23 09:43:27 -0600

    quick fix to user controller, wasn't setting role to member

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    @user.role = Role.find_by_name('member')
     respond_to do |format|
       if @user.save
         flash[:success] = 'User was successfully created.'
